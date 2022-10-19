88f2ff7fd12291ac37d818abf519b579b8d418a2
Ryan Meinzer <ryanmeinzer@Ryans-MacBook-Pro.local>
2022-10-18 22:39:32 -0700

    updating lottery address per updated lottery contract on solidity v0.8.9

--- src/lottery.js
+++ src/lottery.js
@@ -2,9 +2,8 @@ import web3 from './web3'
 const lotteryAddress = process.env.REACT_APP_LOTTERY_ADDRESS
 
 const address = lotteryAddress
-console.log('addy:', address)
 const abi = [
-  {"constant": true, "inputs": [], "name": "manager", "outputs": [{"name": "", "type": "address"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"}, {"constant": true, "inputs": [], "name": "getPlayers", "outputs": [{"name": "", "type": "address[]"}], "payable": false, "stateMutability": "view", "type": "function"}, {"constant": false, "inputs": [], "name": "enter", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function"}, {"constant": true, "inputs": [{"name": "", "type": "uint256"}], "name": "players", "outputs": [{"name": "", "type": "address"}], "payable": false, "stateMutability": "view", "type": "function"}, {"inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor"}
+  {"inputs": [], "stateMutability": "nonpayable", "type": "constructor", "signature": "constructor"}, {"inputs": [], "name": "enter", "outputs": [], "stateMutability": "payable", "type": "function", "payable": true, "signature": "0xe97dcb62"}, {"inputs": [], "name": "getPlayers", "outputs": [{"internalType": "address payable[]", "name": "", "type": "address[]"}], "stateMutability": "view", "type": "function", "constant": true, "signature": "0x8b5b9ccc"}, {"inputs": [], "name": "manager", "outputs": [{"internalType": "address", "name": "", "type": "address"}], "stateMutability": "view", "type": "function", "constant": true, "signature": "0x481c6a75"}, {"inputs": [], "name": "pickWinner", "outputs": [], "stateMutability": "nonpayable", "type": "function", "signature": "0x5d495aea"}, {"inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}], "name": "players", "outputs": [{"internalType": "address payable", "name": "", "type": "address"}], "stateMutability": "view", "type": "function", "constant": true, "signature": "0xf71d96cb"}
 ]
 
 export default new web3.eth.Contract(abi, address)
